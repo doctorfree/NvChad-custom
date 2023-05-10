2e6bd662f3641f32b9c6eef518538243b5fad624
Ronald Record <github@ronrecord.com>
2023-05-10 13:24:29 -0700

    Integrate featureful branch of NvChad example config

---
+++ plugins.lua
@@ -0,0 +1,76 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  {
+	  "phaazon/hop.nvim",
+		branch = "v2",
+		config = function()
+			require("hop").setup()
+		end,
+	},
+
+	-- color scheme
+  {
+	  "folke/tokyonight.nvim",
+		-- commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764",
+		event = "BufWinEnter",
+		config = function()
+			require "custom.configs.tokyonight"
+			-- vim.cmd [[colorscheme tokyonight-night]]
+		end,
+	},
+
+  -- { import = "custom.configs.extras.copilot" },
+  { import = "custom.configs.extras.diffview" },
+  { import = "custom.configs.extras.mason-extras" },
+  { import = "custom.configs.extras.symbols-outline" },
+  { import = "custom.configs.extras.trouble" },
+}
+
+return plugins
